f6b4449d32a637a5371485bc8f5af84dc1fe89ca
Ethan Liu <1230lyx@gmail.com>
2023-11-14 06:03:08 +0800

    last 3 months

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -1,6 +1,7 @@
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 import { Bar } from "react-chartjs-2";
 import { faker } from "@faker-js/faker";
+import dayjs from "dayjs";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
@@ -17,20 +18,23 @@ export const options = {
   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+// let labels = [0, 1, 2, 3, 4, 5, 6].map((i) => {
+//   return dayjs().subtract(i, "day").format("MMM D, YYYY");
+// });
+
+const labels = new Array(3 * 30).fill(0).map((_, i) => {
+  return dayjs().subtract(i, "day").format("MMM D, YYYY");
+});
+
+// labels = lastThreeMonth;
 
 export const data = {
   labels,
   datasets: [
     {
-      label: "Dataset 1",
+      label: "Date",
       data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
       backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.number.int({ min: 0, max: 1000 })),
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
   ],
 };
