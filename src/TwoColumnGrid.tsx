58108a51129ca773e9571fca1fe8b844b7e411b6
Ethan Liu <1230lyx@gmail.com>
2023-11-14 06:03:08 +0800

    last 3 months

--- src/TwoColumnGrid.tsx
+++ src/TwoColumnGrid.tsx
@@ -4,10 +4,10 @@ import { DoughnutChart } from "./DoughnutChart.tsx";
 export function TwoColumnGrid() {
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-1 justify-items-center items-end gap-x-4 gap-y-16 lg:grid-cols-2">
+      <div className="grid grid-cols-1 items-end justify-items-center gap-x-4 gap-y-16 lg:grid-cols-2">
         {[1, 2, 3, 4, 5].map(() => (
           <>
-            <div className="justify-self-stretch h-72">
+            <div className="h-72 justify-self-stretch">
               <BarChart />
             </div>
             <div className="h-60">
